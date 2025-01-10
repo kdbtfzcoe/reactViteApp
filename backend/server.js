ccd3114fcb4b8bec9a7a9f6ff98d953b56d7a642
Jeimarie Casey Pedraya <pedrayacasey08@gmail.com>
2025-01-10 17:16:17 +0800

    feat:

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,22 @@ app.listen(PORT, () => {
 
 */
 
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back the React app.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+
 
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
-    
+
